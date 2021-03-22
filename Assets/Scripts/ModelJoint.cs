de812974d208d3ae7cd8c047332c1f0fab8cc9c6
davidgarsilves <76434709+davidgarsilves@users.noreply.github.com>
2021-03-22 20:03:12 +0100

    Sprint 10
    Avatar integrado con captura de movimiento

---
+++ Assets/Scripts/ModelJoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Microsoft.Azure.Kinect.BodyTracking;
+
+[System.Serializable]
+class ModelJoint
+{
+    public Transform bone;
+    public JointId jointId;
+    [HideInInspector] public Quaternion baseRotOffset;
+}
